ffcf5484f15335831c1c3400936f7ec4c3feac86
Kyle Zentner <zentner.kyle@gmail.com>
2013-04-24 15:52:29 -0700

    Made config_downloader have a main function.

--- config_downloader.py
+++ config_downloader.py
@@ -7,14 +7,18 @@ import os.path
 
 config_dir = 'configs'
 
-for i in range(32):
-    print('Getting', i)
-    target_base = 'https://pioneers.berkeley.edu/team_config/api/{}/'
-    try:
-        f = urllib.urlopen(target_base.format(i))
-        obj = json.load(f)
-        data = obj[u'file_contents']
-        with open(os.path.join(config_dir, '{}.cfg'.format(i)), 'w') as wfile:
-            wfile.write(data)
-    except Exception as ex:
-        print("Couldn't query", target_base.format(i), "got", ex)
+def main():
+    for i in range(32):
+        print('Getting', i)
+        target_base = 'https://pioneers.berkeley.edu/team_config/api/{}/'
+        try:
+            f = urllib.urlopen(target_base.format(i))
+            obj = json.load(f)
+            data = obj[u'file_contents']
+            with open(os.path.join(config_dir, '{}.cfg'.format(i)), 'w') as wfile:
+                wfile.write(data)
+        except Exception as ex:
+            print("Couldn't query", target_base.format(i), "got", ex)
+
+if __name__ == '__main__':
+    main()
