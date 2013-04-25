c2f11786e69938789d35d7f3d7c768119515dd67
Andrew Chen <andrew.chen@berkeley.edu>
2013-04-25 05:08:03 -0700

    reupdated flagging.sh, removed GoalCounter.py

--- flagging.sh
+++ flagging.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 urxvt -title "Flagger" -e sh -c  './Flagger.py'&
-urxvt -title "GoalCounter" -e sh -c  './GoalCounter.py'&
+urxvt -title "goal_vis" -e sh -c  './goal_vis.py'&
 urxvt -title "GoalReader" -e sh -c  './GoalReader.py'&
 urxvt -title "MaestroDriver" -e sh -c  './MaestroDriver/MaestroDriver/bin/Debug/MaestroDriver.exe'
