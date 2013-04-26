9586ab08b8a9737428e9deeeb35e07ccf0e0b748
Kyle Zentner <zentner.kyle@gmail.com>
2013-04-26 12:18:16 -0700

    Made configurator useful to import

--- configurator.py
+++ configurator.py
@@ -17,7 +17,6 @@ def main():
     parser.add_argument('--teams', type=str, action='store')
     parser.add_argument('--config', const=True, action='store_const')
     parser.add_argument('--reset', const=True, action='store_const')
-    parser.add_argument('--run', const=True, action='store_const')
     args = parser.parse_args()
     teams = []
     try:
@@ -26,9 +25,7 @@ def main():
         print('Could not parse team list.')
         print('Please send a comma-separated list of numbers.')
     if args.config:
-        do_config(lc, args, teams)
-    if args.run:
-        do_run(lc, args, teams)
+        do_config(lc, args.load, teams)
     if args.reset:
         do_reset(lc, args, teams)
 
@@ -37,17 +34,14 @@ def do_reset(lc, args, teams):
     for i in range(len(teams)):
         send_team_reset(lc, teams[i], i + 1)
 
-def do_config(lc, args, teams):
-    field_file = args.load or 'resources/field_mapping.json'
+def do_config(lc, field_map_filename, teams):
+    field_file = field_map_filename or 'resources/field_mapping.json'
     field_objects = '[]'
     with open(field_file, 'r') as rfile:
         field_objects = rfile.read()
     #print('Field map', field_objects)
     for i in range(len(teams)):
         send_team_config(lc, teams[i], i+1, field_objects)
-
-def do_run(lc, args, teams):
-    print('Countdown.')
 
 def get_default_config():
     try:
@@ -78,7 +72,7 @@ def get_name(num):
 def send_team_config(lc, num, idx, field_objects):
     data = ConfigData()
     data.ConfigFile = get_config(num)
-    data.IsBlueAlliance = idx < 2
+    data.IsBlueAlliance = idx <= 2
     data.TeamNumber = int(num)
     data.TeamName = get_name(num)
     data.FieldObjects = field_objects
