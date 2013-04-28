c5c3293aa0c2c76555ba2c1f790efba53d1fb31a
Andrew Chen <andrew.chen@berkeley.edu>
2013-04-28 00:15:49 -0700

    send-flags sending Goals/Goals with all goals 1, for testing

--- send-flags.py
+++ send-flags.py
@@ -3,18 +3,40 @@ import lcm
 import time
 
 #from exlcm import example_t
-from Forseti import Flags
-from Forseti import CommandData
+import Forseti
+
 
 lc = lcm.LCM("udpm://239.255.76.67:7667?ttl=1")
 
-msg = CommandData()
-msg.command = "Reset"
 
-#msg = Flags()
-#msg.goals = [[2, 2, 2, 2, 2], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]
 
-lc.publish("PiEMOS1/Command", msg.encode())
-lc.publish("PiEMOS2/Command", msg.encode())
-lc.publish("PiEMOS3/Command", msg.encode())
-lc.publish("PiEMOS4/Command", msg.encode())
+# msg = Forseti.VideoShow()
+# msg.ShowFlags = True
+# msg.ShowTimer = True
+# msg.ShowScore = True
+# msg.ShowShroud = False
+# msg.ShowSchedule = True
+# lc.publish('Video/Show', msg.encode())
+
+'''
+msg = Forseti.VideoVersusText()
+msg.LeftAllianceText = "leftalliancetext"
+msg.RightAllianceText = "rightalliancetext"
+msg.MatchText = "matchtext"
+lc.publish('Video/VersusText', msg.encode())
+'''
+
+msg = Forseti.GoalBoxes()
+msg.goals = [
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1]
+    ]
+lc.publish("Goals/Goals", msg.encode())
+
+
+# msg = Forseti.Score()
+# msg.blue_boxpoints = 10
+# msg.gold_boxpoints = 20
+# lc.publish("ScoreKeeper/Score", msg.encode())
